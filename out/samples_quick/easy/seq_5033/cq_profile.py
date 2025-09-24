debc123742bcdb4a96e2fa5b452584115b1f8830
Era Syla <erasyla@mit.edu>
2025-09-24 17:18:56 -0400

    added some sketches

---
+++ out/samples_quick/easy/seq_5033/cq_profile.py
@@ -0,0 +1,17 @@
+# Auto-generated CadQuery profile for seq 5033 (easy)
+# Open this file in cq-editor. Set EXTRUDE=True to create a STEP.
+import cadquery as cq
+
+EXTRUDE = False
+HEIGHT  = 5.0
+
+def build_profile() -> cq.Workplane:
+    wp = cq.Workplane("XY")
+    # TODO: complex/unsupported profile; edit by hand
+    return wp
+
+if __name__ == "__main__":
+    wp = build_profile()
+    if EXTRUDE:
+        solid = wp.extrude(HEIGHT)
+        cq.exporters.export(solid, "part.step")
